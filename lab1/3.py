22a5c158e7decf83edbc7d69834ee568fae52aec
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-09-12 17:04:43 +0300

    lab1.3++

--- lab1/3.py
+++ lab1/3.py
@@ -2,14 +2,16 @@ import cv2
 
 cv2.namedWindow('video', cv2.WINDOW_AUTOSIZE)
 capture = cv2.VideoCapture(r"videoSample.mp4", cv2.CAP_ANY)
+capture.set(3, 640)
+capture.set(4, 480)
 
 while True:
     ret, frame = capture.read()
 
     if not ret:
         break
-
-    cv2.imshow('video', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('video', gray)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
