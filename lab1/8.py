b04776165f7918872e562713c63990bf65720950
RedMaG02 <redmagwork@gmail.com>
2023-10-18 03:28:33 +0300

    pent

--- lab1/8.py
+++ lab1/8.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 
 def cross(img, color):
     h = img.shape[0]
@@ -20,6 +20,33 @@ def cross(img, color):
 
     cv2.rectangle(img, upper_left, down_right, color, -1)
 
+def le_pentagramm(img, line_length):
+    h = img.shape[0]
+    w = img.shape[1]
+    center_x, center_y = w // 2, h // 2
+    circle_radius = 100
+    cv2.circle(img, (center_x, center_y), circle_radius, (0, 0, 255), thickness=3)
+
+    star_outer_radius = circle_radius
+    star_inner_radius = circle_radius // 2
+    star_points = []
+    for i in range(6):
+        angle = i * 2 * np.pi / 6
+        x = center_x + star_outer_radius * np.cos(angle)
+        y = center_y + star_outer_radius * np.sin(angle)
+        star_points.append((x, y))
+        x = center_x + star_inner_radius * np.cos(angle + np.pi / 6)
+        y = center_y + star_inner_radius * np.sin(angle + np.pi / 6)
+        star_points.append((x, y))
+
+
+    star_points = np.array(star_points, dtype=np.int32)
+
+    for i in range(0, len(star_points), 2):
+        cv2.line(img, tuple(star_points[i]), tuple(star_points[(i + 3) % len(star_points)]), (0, 0, 255), thickness=4)
+        cv2.line(img, tuple(star_points[i + 1]), tuple(star_points[(i + 4) % len(star_points)]), (0, 0, 255),
+                 thickness=4)
+
 def get_color(color):
     if color[0] >= (color[1] and color[2]):
         return (255, 0, 0)
@@ -29,7 +56,7 @@ def get_color(color):
         return (0, 0, 255)
 
 def main():
-    capture = cv2.VideoCapture(r"videoSample.mp4")
+    capture = cv2.VideoCapture(0)
 
     while True:
         ret, frame = capture.read()
@@ -39,7 +66,8 @@ def main():
 
         h = frame.shape[0]
         w = frame.shape[1]
-        cross(frame, get_color(frame[numpy.int16(w / 2), numpy.int16(h / 2)]))
+        #cross(frame, get_color(frame[np.int16(w / 2), np.int16(h / 2)]))
+        le_pentagramm(frame, 30)
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == 27:
